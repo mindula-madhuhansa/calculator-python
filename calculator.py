13ec30357ff825b84cb53d077bb1ff15ea65821d
mindula-madhuhansa <mimmindula@gmail.com>
2023-05-01 20:02:34 +0530

    add volume calculator
    add prism, pyramid, sphere, cone, cylinder functions

--- calculator.py
+++ calculator.py
@@ -7,8 +7,10 @@ isChoosing = True
 
 while isChoosing:
     print("Enter calculator type:")
-    print("\tArithmetic Calculator: 1")
-    print("\tSecondary Calculator: 2")
+    print("\tArithmetic Calculator:\t 1")
+    print("\tBoundary Calculator:\t 2")
+    print("\tArea Calculator:\t\t 3")
+    print("\tVolume Calculator:\t\t 4")
     print()
 
     calc_option = input("Select the calculator: ")
@@ -22,6 +24,7 @@ while isChoosing:
             operator = input("Enter the operator: ")
 
             if operator == "~":
+                print()
                 print("Going back...")
                 print()
                 isCalculating = False
@@ -30,43 +33,140 @@ while isChoosing:
                 print(number1, operator, number2, "=", answer)
                 print()
 
+        isCalculating = True
+
     elif calc_option == "2":
         while isCalculating:
             print("Select an option:")
-            print("\tFind perimeter: 1")
-            print("\tFind area: 2")
-            print("\tFind circumference: 3")
-            print("\tFind area(circle): 4")
+            print("\tFind perimeter of Rectangle: 1")
+            print("\tFind circumference of Circle: 2")
             print()
 
             choice = input("Enter the Choice: ")
+            print()
 
             if choice == "~":
+                print()
                 print("Going back...")
                 print()
                 isCalculating = False
             elif choice == "1":
-                width = int(input("Enter width: "))
-                height = int(input("Enter height: "))
-                answer = operations.perimeter(width, height)
-                print("Perimeter of the rectangle =", answer)
+                answer = operations.perimeter()
+                print("Perimeter of the Rectangle =", answer)
                 print()
             elif choice == "2":
-                width = int(input("Enter width: "))
-                height = int(input("Enter height: "))
-                answer = operations.area_rect(width, height)
-                print("Area of the rectangle =", answer)
+                answer = operations.circumference()
+                print("Circumference of the Circle =", answer)
                 print()
-            elif choice == "3":
-                radius = int(input("Enter radius: "))
-                answer = operations.circumference(radius)
-                print("Circumference of the circle =", answer)
+
+        isCalculating = True
+
+    elif calc_option == "3":
+        while isCalculating:
+            print("Select an option:")
+            print("\tFind area of Rectangle:\t\t 01")
+            print("\tFind area of Circle:\t\t 02")
+            print("\tFind area of Parallelogram:\t 03")
+            print("\tFind area of Triangle:\t\t 04")
+            print("\tFind area of Trapezoid:\t\t 05")
+            print("\tFind area of Prism:\t\t\t 06")
+            print("\tFind area of Pyramid:\t\t 07")
+            print("\tFind area of Cylinder:\t\t 08")
+            print("\tFind area of Sphere:\t\t 09")
+            print("\tFind area of Cone:\t\t\t 10")
+            print()
+
+            choice = input("Enter the Choice: ")
+            print()
+
+            if choice == "~":
                 print()
-            elif choice == "4":
-                radius = int(input("Enter radius: "))
-                answer = operations.area_circle(radius)
-                print("Area of the circle =", answer)
+                print("Going back...")
                 print()
+                isCalculating = False
+            elif choice == "1" or choice == "01":
+                answer = operations.area_rectangle()
+                print("Area of the Rectangle =", answer)
+                print()
+            elif choice == "2" or choice == "02":
+                answer = operations.area_circle()
+                print("Area of the Circle =", answer)
+                print()
+            elif choice == "3" or choice == "03":
+                answer = operations.area_parallelogram()
+                print("Area of the Parallelogram =", answer)
+                print()
+            elif choice == "4" or choice == "04":
+                answer = operations.area_triangle()
+                print("Area of the Triangle =", answer)
+                print()
+            elif choice == "5" or choice == "05":
+                answer = operations.area_trapezoid()
+                print("Area of the Trapezoid =", answer)
+                print()
+            elif choice == "6" or choice == "06":
+                answer = operations.area_prism()
+                print("Area of the Prism =", answer)
+                print()
+            elif choice == "7" or choice == "07":
+                answer = operations.area_pyramid()
+                print("Area of the Pyramid =", answer)
+                print()
+            elif choice == "8" or choice == "08":
+                answer = operations.area_cylinder()
+                print("Area of the Cylinder =", answer)
+                print()
+            elif choice == "9" or choice == "09":
+                answer = operations.area_sphere()
+                print("Area of the Sphere =", answer)
+                print()
+            elif choice == "10":
+                answer = operations.area_cone()
+                print("Area of the Cone =", answer)
+                print()
+
+        isCalculating = True
+
+    elif calc_option == "4":
+        while isCalculating:
+            print("Select an option:")
+            print("\tFind area of Prism:\t\t\t 01")
+            print("\tFind area of Pyramid:\t\t 02")
+            print("\tFind area of Cylinder:\t\t 03")
+            print("\tFind area of Sphere:\t\t 04")
+            print("\tFind area of Cone:\t\t\t 05")
+            print()
+
+            choice = input("Enter the Choice: ")
+            print()
+
+            if choice == "~":
+                print()
+                print("Going back...")
+                print()
+                isCalculating = False
+            elif choice == "1" or choice == "01":
+                answer = operations.volume_prism()
+                print("Volume of the Prism =", answer)
+                print()
+            elif choice == "2" or choice == "02":
+                answer = operations.volume_pyramid()
+                print("Volume of the Pyramid =", answer)
+                print()
+            elif choice == "3" or choice == "03":
+                answer = operations.volume_cylinder()
+                print("Volume of the Cylinder =", answer)
+                print()
+            elif choice == "4" or choice == "04":
+                answer = operations.volume_sphere()
+                print("Volume of the Sphere =", answer)
+                print()
+            elif choice == "5" or choice == "05":
+                answer = operations.volume_cone()
+                print("Volume of the Cone =", answer)
+                print()
+
+        isCalculating = True
 
     elif calc_option == "~":
         print("Simple calculator stopped")
--- operations.py
+++ operations.py
@@ -1,6 +1,8 @@
 from math import pi
 
 
+# Basic Arithmetic Functions
+
 # adding
 def addition(number1, number2):
     return number1 + number2
@@ -31,29 +33,6 @@ def power(number1, number2):
     return number1 ** number2
 
 
-#######################################################
-
-
-# perimeter
-def perimeter(width, height):
-    return (width + height) * 2
-
-
-# area (rectangle)
-def area_rect(width, height):
-    return width * height
-
-
-# circumference
-def circumference(radius):
-    return 2 * pi * radius
-
-
-# area (circle)
-def area_circle(radius):
-    return pi * radius ** 2
-
-
 # calculation
 def calculation(number1, number2, operator):
     if operator == "+":
@@ -70,3 +49,124 @@ def calculation(number1, number2, operator):
         return power(number1, number2)
     else:
         print("Wrong operator")
+
+
+# Boundary Calculator Functions
+
+# perimeter
+def perimeter():
+    width = int(input("Enter Width: "))
+    height = int(input("Enter Height: "))
+    return (width + height) * 2
+
+
+# circumference
+def circumference():
+    radius = int(input("Enter Radius: "))
+    return 2 * pi * radius
+
+
+# Area Calculator Functions
+
+# area (rectangle)
+def area_rectangle():
+    width = int(input("Enter Width: "))
+    height = int(input("Enter Height: "))
+    return width * height
+
+
+# area (circle)
+def area_circle():
+    radius = int(input("Enter Radius: "))
+    return pi * radius ** 2
+
+
+# area (parallelogram)
+def area_parallelogram():
+    base = int(input("Enter Base Length: "))
+    height = int(input("Enter Height: "))
+    return base * height
+
+
+# area (parallelogram)
+def area_triangle():
+    base = int(input("Enter Base Length: "))
+    height = int(input("Enter Height: "))
+    return (base * height) / 2
+
+
+# area (parallelogram)
+def area_trapezoid():
+    base1 = int(input("Enter Base Length 1: "))
+    base2 = int(input("Enter Base Length 2: "))
+    height = int(input("Enter Height: "))
+    return (base1 + base2) * height / 2
+
+
+# area (prism)
+def area_prism():
+    width = int(input("Enter Base Width: "))
+    length = int(input("Enter Base Length: "))
+    height = int(input("Enter Height: "))
+    return 2 * width * length + (width + length) * 2 * height
+
+
+# area (pyramid)
+def area_pyramid():
+    width = int(input("Enter Base Width: "))
+    length = int(input("Enter Base Length: "))
+    slant = int(input("Enter Slant Height: "))
+    return (width * length) + slant * (width + length)
+
+
+# area (cylinder)
+def area_cylinder():
+    radius = int(input("Enter Radius: "))
+    height = int(input("Enter Height: "))
+    return (2 * pi * radius ** 2) + (2 * pi * radius * height)
+
+
+# area (sphere)
+def area_sphere():
+    radius = int(input("Enter Radius: "))
+    return 4 * pi * radius ** 2
+
+
+# area (cone)
+def area_cone():
+    radius = int(input("Enter Radius: "))
+    slant = int(input("Enter Slant Height: "))
+    return (pi * radius ** 2) + (pi * radius * slant)
+
+
+# Volume Calculator Functions
+
+# volume (prism)
+def volume_prism():
+    width = int(input("Enter Base Width: "))
+    length = int(input("Enter Base Length: "))
+    height = int(input("Enter Height: "))
+    return width * length * height
+
+
+# volume (pyramid)
+def volume_pyramid():
+    return volume_prism() / 3
+
+
+# volume (cylinder)
+def volume_cylinder():
+    base_area = area_circle()
+    height = int(input("Enter Height: "))
+    return base_area * height
+
+
+# volume (sphere)
+def volume_sphere():
+    radius = int(input("Enter Radius: "))
+    return 4 / 3 * (pi * radius ** 3)
+
+
+# volume (cone)
+def volume_cone():
+    return volume_cylinder() / 3
