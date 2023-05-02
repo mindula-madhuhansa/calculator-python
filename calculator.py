ee1fd5a362b9937a7abb56a7cc3a1cda9ea20b9d
mindula-madhuhansa <mimmindula@gmail.com>
2023-05-02 23:06:13 +0530

    add package

--- calculator.py
+++ calculator.py
@@ -1,4 +1,17 @@
-import operations
+import calculatorpackage.standardcalculator as standard
+import calculatorpackage.shapecalculator as shape
+import os
+
+
+# clear console
+def clear_console():
+    # Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # Mac/Linux
+    else:
+        _ = os.system('clear')
+
 
 print("*** Simple Calculator ***")
 print()
@@ -6,17 +19,22 @@ isCalculating = True
 isChoosing = True
 
 while isChoosing:
-    print("Enter calculator type:")
-    print("\tArithmetic Calculator:\t 1")
-    print("\tBoundary Calculator:\t 2")
-    print("\tArea Calculator:\t\t 3")
-    print("\tVolume Calculator:\t\t 4")
+    print("Calculator")
+    print("\tStandard:\t\t 01")
+    print("\tBoundary:\t\t 02")
+    print("\tArea:\t\t\t 03")
+    print("\tVolume:\t\t\t 04")
+    print("\tProgrammer:\t\t 05")
+    print()
+    print("Converter")
+    print("\tLength:\t\t\t 06")
     print()
 
-    calc_option = input("Select the calculator: ")
+    calc_option = input("Select Calculator/Converter: ")
     print()
 
-    if calc_option == "1":
+    if calc_option == "1" or calc_option == "01":
+        clear_console()
         while isCalculating:
             number1 = int(input("Enter number 1: "))
             number2 = int(input("Enter number 2: "))
@@ -29,13 +47,15 @@ while isChoosing:
                 print()
                 isCalculating = False
             else:
-                answer = operations.calculation(number1, number2, operator)
+                answer = standard.calculation(number1, number2, operator)
                 print(number1, operator, number2, "=", answer)
                 print()
 
+        clear_console()
         isCalculating = True
 
-    elif calc_option == "2":
+    elif calc_option == "2" or calc_option == "02":
+        clear_console()
         while isCalculating:
             print("Select an option:")
             print("\tFind perimeter of Rectangle: 1")
@@ -51,17 +71,19 @@ while isChoosing:
                 print()
                 isCalculating = False
             elif choice == "1":
-                answer = operations.perimeter()
+                answer = shape.perimeter()
                 print("Perimeter of the Rectangle =", answer)
                 print()
             elif choice == "2":
-                answer = operations.circumference()
+                answer = shape.circumference()
                 print("Circumference of the Circle =", answer)
                 print()
 
+        clear_console()
         isCalculating = True
 
-    elif calc_option == "3":
+    elif calc_option == "3" or calc_option == "03":
+        clear_console()
         while isCalculating:
             print("Select an option:")
             print("\tFind area of Rectangle:\t\t 01")
@@ -69,11 +91,11 @@ while isChoosing:
             print("\tFind area of Parallelogram:\t 03")
             print("\tFind area of Triangle:\t\t 04")
             print("\tFind area of Trapezoid:\t\t 05")
-            print("\tFind area of Prism:\t\t\t 06")
+            print("\tFind area of Prism:\t\t 06")
             print("\tFind area of Pyramid:\t\t 07")
             print("\tFind area of Cylinder:\t\t 08")
             print("\tFind area of Sphere:\t\t 09")
-            print("\tFind area of Cone:\t\t\t 10")
+            print("\tFind area of Cone:\t\t 10")
             print()
 
             choice = input("Enter the Choice: ")
@@ -85,56 +107,58 @@ while isChoosing:
                 print()
                 isCalculating = False
             elif choice == "1" or choice == "01":
-                answer = operations.area_rectangle()
+                answer = shape.area_rectangle()
                 print("Area of the Rectangle =", answer)
                 print()
             elif choice == "2" or choice == "02":
-                answer = operations.area_circle()
+                answer = shape.area_circle()
                 print("Area of the Circle =", answer)
                 print()
             elif choice == "3" or choice == "03":
-                answer = operations.area_parallelogram()
+                answer = shape.area_parallelogram()
                 print("Area of the Parallelogram =", answer)
                 print()
             elif choice == "4" or choice == "04":
-                answer = operations.area_triangle()
+                answer = shape.area_triangle()
                 print("Area of the Triangle =", answer)
                 print()
             elif choice == "5" or choice == "05":
-                answer = operations.area_trapezoid()
+                answer = shape.area_trapezoid()
                 print("Area of the Trapezoid =", answer)
                 print()
             elif choice == "6" or choice == "06":
-                answer = operations.area_prism()
+                answer = shape.area_prism()
                 print("Area of the Prism =", answer)
                 print()
             elif choice == "7" or choice == "07":
-                answer = operations.area_pyramid()
+                answer = shape.area_pyramid()
                 print("Area of the Pyramid =", answer)
                 print()
             elif choice == "8" or choice == "08":
-                answer = operations.area_cylinder()
+                answer = shape.area_cylinder()
                 print("Area of the Cylinder =", answer)
                 print()
             elif choice == "9" or choice == "09":
-                answer = operations.area_sphere()
+                answer = shape.area_sphere()
                 print("Area of the Sphere =", answer)
                 print()
             elif choice == "10":
-                answer = operations.area_cone()
+                answer = shape.area_cone()
                 print("Area of the Cone =", answer)
                 print()
 
+        clear_console()
         isCalculating = True
 
-    elif calc_option == "4":
+    elif calc_option == "4" or calc_option == "04":
+        clear_console()
         while isCalculating:
             print("Select an option:")
-            print("\tFind area of Prism:\t\t\t 01")
-            print("\tFind area of Pyramid:\t\t 02")
-            print("\tFind area of Cylinder:\t\t 03")
-            print("\tFind area of Sphere:\t\t 04")
-            print("\tFind area of Cone:\t\t\t 05")
+            print("\tFind volume of Prism:\t\t 01")
+            print("\tFind volume of Pyramid:\t\t 02")
+            print("\tFind volume of Cylinder:\t 03")
+            print("\tFind volume of Sphere:\t\t 04")
+            print("\tFind volume of Cone:\t\t 05")
             print()
 
             choice = input("Enter the Choice: ")
@@ -146,25 +170,64 @@ while isChoosing:
                 print()
                 isCalculating = False
             elif choice == "1" or choice == "01":
-                answer = operations.volume_prism()
+                answer = shape.volume_prism()
                 print("Volume of the Prism =", answer)
                 print()
             elif choice == "2" or choice == "02":
-                answer = operations.volume_pyramid()
+                answer = shape.volume_pyramid()
                 print("Volume of the Pyramid =", answer)
                 print()
             elif choice == "3" or choice == "03":
-                answer = operations.volume_cylinder()
+                answer = shape.volume_cylinder()
                 print("Volume of the Cylinder =", answer)
                 print()
             elif choice == "4" or choice == "04":
-                answer = operations.volume_sphere()
+                answer = shape.volume_sphere()
                 print("Volume of the Sphere =", answer)
                 print()
             elif choice == "5" or choice == "05":
-                answer = operations.volume_cone()
+                answer = shape.volume_cone()
                 print("Volume of the Cone =", answer)
                 print()
+
+        clear_console()
+        isCalculating = True
+
+    elif calc_option == "5" or calc_option == "05":
+        clear_console()
+        pass
+
+    elif calc_option == "6" or calc_option == "06":
+        clear_console()
+        while isCalculating:
+            print("Select the converter unit:")
+            print("Picometers(pm):\t\t 01")
+            print("Nanometers(nm):\t\t 02")
+            print("Micrometers(µm):\t 03")
+            print("Millimeters(mm):\t 04")
+            print("Centimeters(cm):\t 05")
+            print("Meters(m):\t\t 06")
+            print("Kilometers(km):\t\t 07")
+            print("Inches(in):\t\t 08")
+            print("Feets(ft):\t\t 09")
+            print("Yards(yd):\t\t 10")
+            print("Miles(mi):\t\t 11")
+            print("Nautical Miles(NM):\t 12")
+            print()
+
+            converter_unit = input("Enter Converter unit: ")
+            if converter_unit == "~":
+                print()
+                print("Going back...")
+                print()
+                isCalculating = False
+            else:
+                converted_unit = input("Enter Converted unit: ")
+                if converted_unit == "~":
+                    print()
+                    print("Going back...")
+                    print()
+                    isCalculating = False
 
         isCalculating = True
 
--- calculatorpackage/shapecalculator.py
+++ calculatorpackage/shapecalculator.py
@@ -1,56 +1,6 @@
 from math import pi
 
 
-# Basic Arithmetic Functions
-
-# adding
-def addition(number1, number2):
-    return number1 + number2
-
-
-# subtracting
-def subtraction(number1, number2):
-    return number1 - number2
-
-
-# multiplying
-def multiplication(number1, number2):
-    return number1 * number2
-
-
-# dividing
-def division(number1, number2):
-    return number1 / number2
-
-
-# modulus
-def modulus(number1, number2):
-    return number1 % number2
-
-
-# power
-def power(number1, number2):
-    return number1 ** number2
-
-
-# calculation
-def calculation(number1, number2, operator):
-    if operator == "+":
-        return addition(number1, number2)
-    elif operator == "-":
-        return subtraction(number1, number2)
-    elif operator == "*":
-        return multiplication(number1, number2)
-    elif operator == "/":
-        return division(number1, number2)
-    elif operator == "%":
-        return modulus(number1, number2)
-    elif operator == "^":
-        return power(number1, number2)
-    else:
-        print("Wrong operator")
-
-
 # Boundary Calculator Functions
 
 # perimeter
